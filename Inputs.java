42678fc763dee51e0672600229d29892257d6e91
shre4ash <shreyashkumbhar9@outlook.com>
2025-01-20 19:22:52 +0530

    input file

---
+++ Inputs.java
@@ -0,0 +1,10 @@
+
+
+public class Inputs{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter some input: ");
+        int rollno = input.nextInt();
+        System.out.println("your roll no "+ rollno)
+    }
+}
