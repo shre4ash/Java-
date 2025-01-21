19163b7f8caf4029286c6d5ff902b4a6c448d3f4
shre4ash <shreyashkumbhar9@outlook.com>
2025-01-21 19:38:30 +0530

    types in Java

---
+++ Java Basics/Types.java
@@ -0,0 +1,27 @@
+public class Types{
+    public static void main(String[] args){
+        //////////PRIMITIVE DATA TYPES//////////
+        //byte - 1 [-128 to 127]
+        //short - 2 bytes
+        //int - 4 bytes
+        //long - 8 bytes
+        //float - 4 bytes 3.14
+        //double - 8 bytes
+        //char - 2 bytes   a,b,c,d
+        //boolean - 1 bit true / false
+
+        // byte  age = 30;
+        // int phone = 95611545929;
+        // long phone2 = 95611545929L;
+        // float pi = 3.14f;
+        // char letter = '@';
+        // boolean isAdult = true;
+
+
+    //////////NON-PRIMITIVE DATA TYPES//////////
+
+    String name = "shreyash";
+    System.out.println(name.length());
+
+    }
+}
