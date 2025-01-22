21c5413532fc7ad465bf71aa135946d2b091ed85
shre4ash <shreyashkumbhar9@outlook.com>
2025-01-22 18:57:13 +0530

    strings in java

---
+++ Java Basics/String.java
@@ -0,0 +1,18 @@
+import java.util.Scanner   // import the Scanner class
+
+public class String {
+   public static void main(String[] args){
+
+       //Strings
+       //concatenate
+    //    String name1 = "shreyash";
+    //    String name2 = "apu";
+    //    String name3 = name1 + name2;
+    //    System.out.println(name3);
+
+
+    // charAt
+    String name = "shreyash";
+    System.out.println(name.charAt(1));
+   }
+}
