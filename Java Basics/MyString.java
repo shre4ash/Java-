2fb3d2054903ec9bab13383dafda992a3fc4d6ec
shre4ash <shreyashkumbhar9@outlook.com>
2025-01-23 07:24:37 +0530

    Changes

---
+++ Java Basics/MyString.java
@@ -0,0 +1,35 @@
+import java.util.Scanner ;  // import the Scanner class
+
+public class MyString {
+   public static void main(String[] args){
+    //String 
+    //conatinate
+//      String name1 = "John";
+//      String name2 = "Doe";
+//      String name3 = name1 + " " + name2;
+//      System.out.println(name3);
+   //chatAt
+//      String name= "sheyash";
+//      System.out.println(name.charAt(0));
+
+      //Length
+//      String name= "sheyash";
+//      System.out.println(name.length());
+
+      //Replace
+//      String name = "ahreyash";
+//      String name2 = name.replace( "a","S");
+//      System.out.println(name2);
+//      System.out.println(name);
+
+      // in JAVA string are immutable
+
+// Substring
+      // For extracting the small part from the string
+      // in substring the first index will include but the last one is not include
+      String name = "shreyash and  kumbhar";
+      System.out.println(name.substring(0,4));
+
+
+   }
+}
