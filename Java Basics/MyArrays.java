0483c4cab4be6d6030869310d918b810fb2c41cb
shre4ash <shreyashkumbhar9@outlook.com>
2025-01-23 19:50:06 +0530

    Arrays in java

---
+++ Java Basics/MyArrays.java
@@ -0,0 +1,27 @@
+public class MyArrays {
+    public static void main(String[] args){
+
+//        int age = 30;
+//        int pyhsics = 97;
+//        int chem = 98;
+//        int eng = 95;
+//
+//        int[] marks = new int[3];
+//        marks[0]= 30;
+//        marks[1] = 40;
+//        marks[2] = 50;
+        // length
+//System.out.println(marks.length);
+
+//     Sort
+//        System.out.println(marks[0]);
+//        MyArrays.sort(marks);
+//        System.out.println(marks[0]);
+
+
+        // 2D Arrays
+        int[] marks ={97, 98, 95};
+        int[][] finalMarks = {{9,98,95},{89,33,44}};
+        System.out.println(finalMarks[0][0]);
+    }
+}
