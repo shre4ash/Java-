7cb8cc4dc70e89133eff4f760d800962d66f3c9d
shre4ash <shreyashkumbhar9@outlook.com>
2025-01-30 19:02:03 +0530

    ..

---
+++ Functions and Methods/src/Methods.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Methods{
+    public static void main(String[] args) {
+        //Q: Take a input of two num and print the sum
+
+//        Scanner in = new Scanner(System.in);
+
+//        System.out.println("Enter number 1:");
+//        int num1 = in.nextInt();
+//        System.out.println("Enter number 2:");
+//        int num2 = in.nextInt();
+//        int sum = num1 + num2;
+//        System.out.println("The sum = " + sum);
+
+        sum();
+    }
+         static void sum() {
+            Scanner in = new Scanner(System.in);
+             System.out.println("Enter number 1:");
+        int num1 = in.nextInt();
+        System.out.println("Enter number 2:");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        System.out.println("The sum = " + sum);
+
+        }
+
+
+}
