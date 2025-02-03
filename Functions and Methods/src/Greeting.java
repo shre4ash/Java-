142456561c797b47dedb1a6014aea9a05362c3c0
shre4ash <shreyashkumbhar9@outlook.com>
2025-02-03 18:43:47 +0530

    Swapping in java

---
+++ Functions and Methods/src/Greeting.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Greeting{
+    public static void main(String[] args){
+//         String message = greet();
+//         System.out.println(message);
+//        int a = 10;
+//        int b = 20;
+//int temp = a;
+//a = b;
+//b = temp;
+//
+//System.out.println(a + "                " + b);
+//
+//    }
+
+    // return the value
+
+//    static void greeting(){
+//        System.out.println("hello Shreyash");
+//    }
+
+//    static String greet() {
+//        String greeting = "HOw are you";
+//        return greeting;
+
+        int a = 10;
+        int b = 3;
+
+        static void swap(int a,int b) {
+            int temp = a;
+                a = b;
+                b = temp;
+
+        }
+    }
+}
