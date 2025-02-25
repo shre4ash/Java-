1d3f6403776a148ca809ef787e3948ac2da44b0b
shre4ash <shreyashkumbhar9@outlook.com>
2025-02-25 22:48:01 +0530

    Arrays in JAVA

---
+++ Arrays in java/Arrays.java
@@ -0,0 +1,29 @@
+public class Arrays {
+    public static void main(String[] args){
+        //Q: stroe the roll number 
+
+        int a = 19;
+
+        // Q: store the persons roll number
+        String name = "shreyash kumbhar";
+
+        // // Q: store 5 roll numbers
+        // int roll : 23;
+        // int roll : 55;
+        // int roll : 44;
+
+        // Syntax
+        // datatype[] variable_name = new datatype[size];
+        // store 5 roll numbers:
+        // int[] rnos = new int[5];
+
+        // // or directly
+        // int[] rnos2 = {32, 23, 54, 6, 77};   
+
+        int[] ros; // declaration os array. ros is getting defined in the stack 
+        ros = new int[5]; // initialisatin: actually here object is being created in the memorry
+
+     String[] arr = new String[4];
+    System.out.println(ros[1]);
+    }
+}
